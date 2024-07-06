26ed5134895bad2e735362fc14eaf3704f68eaad
fqidz <meowthful127@gmail.com>
2024-07-06 21:26:48 +0300

    tsfs

--- src/bin/parser.rs
+++ src/bin/parser.rs
@@ -1,74 +1,79 @@
+use std::{io::Error, thread::current};
 fn main() {
     // println!("{}", parse("1/3+(2+3)*(4+5)".to_string()));
-    tokenize("1.003/3+(2+3)*(4+5)".to_string());
+    // tokenize("1.003/3+(2+3)*(4+5)".to_string());
+    println!("{:?}", tokenizer("111123232h993".to_string()));
 }
 
-enum TokenType {
+#[derive(Debug)]
+enum TokenKind {
     Operator,
     Num,
     LParen,
     RParen,
-    Whitespace
+    Whitespace,
 }
 
+#[derive(Debug)]
 struct Token {
     literal: String,
-    token_type: TokenType
+    token_type: TokenKind,
 }
 
-fn tokenize(input: String) -> Vec<(Token, TokenType)> {
-    let result: Vec<(Token, TokenType)> = Vec::new();
-    let input_vec: Vec<char> = input.chars().collect::<Vec<char>>();
-    for index in 0..input.len() {
-        let mut peek_index: usize = 1;
-        let mut token: String;
-        let mut token_type: TokenType;
-        loop {
-            match input_vec[index + peek_index] {
-                '.' => peek_index += 1,
-                c if c.is_whitespace() => {
-                    token = input_vec[index..peek_index - 1].into_iter().collect::<String>();
-                },
-            }
-        }
-    }
-    let mut index: usize;
-    while index < input.len() {
-        let starting_index: usize = index;
-        let mut token_type: TokenType;
-        match input_vec[index] {
-            c if c.is_numeric() => token_type = TokenType::Num,
-            c if 
-                c == '+' ||
-                c == '-' || 
-                c == '+' || 
-                c == '/' => token_type = TokenType::Operator,
-            c if c.is_whitespace() => token_type = TokenType::Whitespace,
-            '(' => token_type = TokenType::LParen,
-            ')' => token_type = TokenType::RParen,
-            _ => panic!()
-        }
-        index += 1;
-        loop {
-            match input_vec[index] {
-                c if c.is_whitespace() => break,
-                '.' => index += 1,
-                c if c.is_numeric() => index += 1
-                _ => panic!()
-            }
-        }
-        let token: String;
-    }
-    
-
-    return result;
-}
-
-fn parse(input: String) -> String {
-    let input: String = input
+fn tokenizer(string: String) -> Result<Vec<Token>, String> {
+    let mut tokens: Vec<Token> = Vec::new();
+    let input_string: Vec<char> = string
         .trim()
-        .parse::<String>()
-        .unwrap();
+        .chars()
+        .filter(|c| !c.is_whitespace())
+        .collect::<Vec<char>>();
 
-    return "".to_string();
+    let mut cur_pos: usize = 0;
+    while cur_pos < input_string.len() {
+        println!("{cur_pos}");
+        let start_pos: usize = cur_pos;
+        match input_string[cur_pos] {
+            c if c.is_numeric() => {
+                while cur_pos < input_string.len() && input_string[cur_pos].is_numeric() {
+                    cur_pos += 1;
+                }
+                tokens.push(Token{
+                    literal: input_string[start_pos..cur_pos]
+                        .iter()
+                        .collect::<String>(),
+                    token_type: TokenKind::Num,
+                });
+            }
+            c if
+                c == '+' ||
+                c == '-' ||
+                c == '*' ||
+                c == '/' => {
+                    tokens.push(Token{
+                        literal: input_string[cur_pos].to_string(),
+                        token_type: TokenKind::Operator,
+                    });
+                }
+            c if c == '(' => {
+                tokens.push(Token{
+                    literal: input_string[cur_pos].to_string(),
+                    token_type: TokenKind::LParen,
+                });
+            }
+            _ => return Err("Invalid Character".to_string()),
+        }
+        cur_pos += 1;
+    }
+    return Ok(tokens);
+}
+
+
+trait Tokenize {
+    fn tokenize(&self);
+}
+
+impl Tokenize for String {
+    fn tokenize(&self) {
+        println!("{self}");
+    }
 }
