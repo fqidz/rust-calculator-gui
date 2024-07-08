bb95755930f0d62b48466df1a829720ebefb421f
fqidz <meowthful127@gmail.com>
2024-07-09 00:12:26 +0300

    changes

--- build.rs
+++ build.rs
@@ -1,5 +1,3 @@
-#![windows_subsystem = "windows"]
-// cargo rustc --release -- -Clink-args="/SUBSYSTEM:WINDOWS /ENTRY:mainCRTStartup"
 // cargo rustc --release --bin rust-calculator-gui -- -Clink-args="/SUBSYSTEM:WINDOWS /ENTRY:mainCRTStartup"
 fn main() {
     slint_build::compile("ui/appwindow.slint").unwrap();
